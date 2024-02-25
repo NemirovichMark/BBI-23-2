4d2a12df18a308ff16795b167b417aa5802c4af2
StrelnikovAlexander <159521548+StrelnikovAlexander@users.noreply.github.com>
2024-02-25 13:20:55 +0300

    lab6 done

---
+++ lab6.2.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Globalization;
+//task #2
+namespace lab6
+{
+    public struct Student
+    {
+        private string _name;
+        private int _markMath;
+        private int _markPhys;
+        private int _markRus;
+
+        public Student(string name, int markMath, int markPhys, int markRus)
+        {
+            _name = name;
+            _markMath = markMath;
+            _markPhys = markPhys;
+            _markRus = markRus;
+        }
+
+        public int MarkMath => _markMath;
+
+        public int MarkPhys => _markPhys;
+
+        public int MarkRus => _markRus;
+
+        public void WriteStudent()
+        {
+            Console.WriteLine($"{_name} Math: {_markMath} Physics: {_markPhys} Russian: {_markRus}");
+        }
+
+        public bool Passed => (_markMath != 2 && _markPhys != 2 && _markRus != 2);
+
+        public double Average => (_markMath + _markPhys + _markRus) / 3.0;
+    }
+
+    internal static class Program
+    {
+        static void Main()
+        {
+            //creating a list of students
+            Student[] students = new Student[5]
+            {
+                new Student("Alex", 2, 3, 4),
+                new Student("Mark", 3, 3, 5),
+                new Student("John", 4, 3, 5),
+                new Student("Sam", 4, 3, 2),
+                new Student("Pete", 5, 5, 4)
+            };
+
+            //counting passed students
+            int countPassed = 0;
+            for (int i = 0; i < students.Length; i++)
+            {
+                if (students[i].Passed)
+                    countPassed++;
+            }
+
+            //creating an array of passed students
+            Student[] PassedStudents= new Student[countPassed];
+            int j = 0;
+            for (int i = 0; i < students.Length; i++)
+                if (students[i].Passed)
+                {
+                    PassedStudents[j] = students[i];
+                    j++;
+                }
+
+            //sorting an array of passed students
+            for (int i = 0; i < PassedStudents.Length - 1; i++)
+                for (int k = 0; k < PassedStudents.Length - i - 1; k++)
+                    if (PassedStudents[k].Average < PassedStudents[k + 1].Average)
+                        (PassedStudents[k], PassedStudents[k + 1]) = (PassedStudents[k + 1], PassedStudents[k]);
+
+            //writing sorted array of passed students
+            for (int i = 0; i < PassedStudents.Length; i++)
+                PassedStudents[i].WriteStudent();
+        }
+    }
+}
---
+++ lab6.3.cs
@@ -0,0 +1,85 @@
+﻿using lab6;
+using System;
+using System.Globalization;
+//task #2
+namespace lab6
+{
+    public struct Team
+    {
+        public int _teamID;
+        public int _score;
+        public Team(int teamID, int score)
+        {
+            _teamID = teamID;
+            _score = score;
+        }
+
+        public int Score => _score;
+        public int TeamID => _teamID;
+
+        public void WriteTeam()
+        {
+            Console.WriteLine($"TeamID: {_teamID} Score: {_score}");
+        }
+    }
+    }
+
+    internal static class Program
+    {
+        static void Main()
+        {
+        //creating 2 groups
+        Team[] Group1 = new Team[12]
+        {
+            new Team(0, 3),
+            new Team(1, 1),
+            new Team(2, 2),
+            new Team(3, 6),
+            new Team(4, 4),
+            new Team(5, 3),
+            new Team(6, 2),
+            new Team(7, 8),
+            new Team(8, 5),
+            new Team(9, 5),
+            new Team(10, 1),
+            new Team(11, 2)
+        };
+
+        Team[] Group2 = new Team[12]
+        {
+            new Team(12, 6),
+            new Team(13, 6),
+            new Team(14, 4),
+            new Team(15, 3),
+            new Team(16, 1),
+            new Team(17, 9),
+            new Team(18, 5),
+            new Team(19, 2),
+            new Team(20, 5),
+            new Team(21, 1),
+            new Team(22, 3),
+            new Team(23, 4)
+        };
+
+        //sorting groups by teams scores
+        for (int i = 0; i < Group1.Length - 1; i++)
+            for (int k = 0; k < Group1.Length - i - 1; k++)
+                if (Group1[k].Score < Group1[k + 1].Score)
+                    (Group1[k], Group1[k + 1]) = (Group1[k + 1], Group1[k]);
+
+        for (int i = 0; i < Group2.Length - 1; i++)
+            for (int k = 0; k < Group1.Length - i - 1; k++)
+                if (Group2[k].Score < Group2[k + 1].Score)
+                    (Group2[k], Group2[k + 1]) = (Group2[k + 1], Group2[k]);
+
+        //writing 2 lists of top-6 teams from both groups
+        for (int i = 0; i < 6; i++)
+        {
+            Group1[i].WriteTeam();
+        }
+        for (int i = 0; i < 6; i++)
+        {
+            Group2[i].WriteTeam();
+        }
+    }
+}
