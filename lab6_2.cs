14210398805d057dd3ad5c1ecfe792209cabe0fc
OsipovPawa <159521611+OsipovPawa@users.noreply.github.com>
2024-02-28 21:16:37 +0300

    Add files via upload

---
+++ lab6_2.cs
@@ -0,0 +1,88 @@
+﻿
+using System;
+using System.Xml.Linq;
+
+struct Student
+{
+    private string _Name;
+    private string _Surname;
+    private int _Math;
+    private int _Phys;
+    private int _Russ;
+
+
+    public string Name => _Name;
+    public string Surname => _Surname;
+    public int Math => _Math;
+    public int Phys => _Phys;
+    public int Russ => _Russ;
+    public double Average => (_Math + _Phys + _Russ) / 3.0;
+
+    public Student(string name, string surname, int math, int phys, int russ)
+    {
+        _Name = name;
+        _Surname = surname;
+        _Math = math;
+        _Phys = phys;
+        _Russ = russ;
+
+        if (math == 2 ^ russ == 2 ^ phys == 2)
+        {
+            _Math = 0;
+            _Russ = 0;
+            _Phys = 0;
+        }
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"{_Name} {_Surname} - средний балл: {Average}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student[] students = new Student[5]
+        {
+            new Student("Ivan", "Ivanov", 3, 4, 5),
+            new Student("Igor", "Igorev", 4, 5, 4),
+            new Student("Volga", "Motors", 5, 5, 5),
+            new Student("Erik", "Davidov", 2, 3, 4),
+            new Student("Face", "Eshkere", 4, 3, 2),
+        };
+
+        Student[] successfulStudents = new Student[students.Length];
+        int successfulCount = 0;
+
+        foreach (var student in students)
+        {
+            if (student.Average >= 3.5)
+            {
+                successfulStudents[successfulCount] = student;
+                successfulCount++;
+            }
+        }
+
+        for (int i = 0; i < successfulCount - 1; i++)
+        {
+            for (int j = 0; j < successfulCount - 1 - i; j++)
+            {
+                if (successfulStudents[j].Average < successfulStudents[j + 1].Average)
+                {
+                    var temp = successfulStudents[j];
+                    successfulStudents[j] = successfulStudents[j + 1];
+                    successfulStudents[j + 1] = temp;
+                }
+            }
+        }
+
+        Console.WriteLine("Отсортированный список успешно сдавших экзамены учащихся:");
+        for (int i = 0; i < successfulCount; i++)
+        {
+            successfulStudents[i].Print();
+        }
+        Console.ReadKey();
+    }
+}
---
+++ lab6_3.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+struct Team
+{
+    private string name;
+    private int _zabit;
+    private int _propusk;
+    private int _ochki;
+
+    public string Name => name;
+    public int Zabit => _zabit;
+    public int Propusk => _propusk;
+    public int Ochki => _ochki;
+
+    public Team(string name, int Zabit, int Propusk)
+    {
+        this.name = name;
+        this._zabit = Zabit;
+        this._propusk = Propusk;
+        if (Zabit > Propusk)
+        {
+            _ochki = 3;
+        }
+        else if (Zabit == Propusk)
+        {
+            _ochki = 1;
+        }
+        else
+        {
+            _ochki = 0;
+        }
+    }
+
+    public string PrintTeamInfo()
+    {
+        return $"{Name}: {Ochki} очков";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Team[] teams = new Team[5];
+        teams[0] = new Team("Винлайн", 1, 1);
+        teams[1] = new Team("ВанВин", 3, 1);
+        teams[2] = new Team("БЕТБУМ", 4, 0);
+        teams[3] = new Team("Крылья Советов", 2, 3);
+        teams[4] = new Team("Енисей", 2, 2);
+
+        for (int i = 0; i < teams.Length - 1; i++)
+        {
+            for (int j = i + 1; j < teams.Length; j++)
+            {
+
+                if (teams[i].Ochki < teams[j].Ochki)
+                {
+                    Team temp = teams[i];
+                    teams[i] = teams[j];
+                    teams[j] = temp;
+                }
+                else if (teams[i].Ochki == teams[j].Ochki)
+                {
+                    int diff1 = teams[i].Zabit - teams[i].Propusk;
+                    int diff2 = teams[j].Zabit - teams[j].Propusk;
+                    if (diff1 < diff2)
+                    {
+                        Team temp = teams[i];
+                        teams[i] = teams[j];
+                        teams[j] = temp;
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine("Таблица результатов:");
+        for (int i = 0; i < teams.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}. {teams[i].PrintTeamInfo()}");
+        }
+    }
+}
