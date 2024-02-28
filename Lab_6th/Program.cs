267932f78c6900212361f6ff8fd771a7ff1cc37f
Кирилл Цыпляев <m2304543@edu.misis.ru>
2024-02-28 20:45:01 +0300

    Вторая часть четвертый номер(прошлый был с ошибкой)

--- Lab_6th/Program.cs
+++ Lab_6th/Program.cs
@@ -2,121 +2,114 @@
 
 namespace lab_6th
 {
-    using System.Drawing;
-    using System.Globalization;
-
-    namespace ConsoleApp1
+    public struct Sportsmen
     {
-        public struct Sportsmen
+        private string _surname;
+        double _points;
+        public Sportsmen(string surname, double points)
         {
-            private string _surname;
-            double _points;
-            public Sportsmen(string surname, double points)
-            {
-                _surname = surname;
-                _points = points;
-            }
-            public double Points
-            { get { return _points; } }
-
-            public string Surname
-            { get { return _surname; } }
-
+            _surname = surname;
+            _points = points;
         }
-        internal class Program
+        public double Points
+        { get { return _points; } }
+
+        public string Surname
+        { get { return _surname; } }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
-            {
-                Sportsmen[] list_of_sportsmens = {
+            Sportsmen[] list_of_sportsmens = {
                 new Sportsmen("Tsoy", Point_Generator()),
                 new Sportsmen("Letov", Point_Generator()),
                 new Sportsmen("Klinskih", Point_Generator()),
                 new Sportsmen("Osbourne", Point_Generator()) };
-                for (int i = 0; i < list_of_sportsmens.Length; i++)
-                {
-                    Console.WriteLine($"имя: {list_of_sportsmens[i].Surname} очки: {list_of_sportsmens[i].Points}");
-                }
-                Sort(list_of_sportsmens);
-                Console.WriteLine("список спортсменов по местам");
-                for (int i = 0; i < list_of_sportsmens.Length; i++)
-                {
-                    Console.WriteLine($"имя: {list_of_sportsmens[i].Surname} очки: {list_of_sportsmens[i].Points}");
-                }
-
-            }
-            static double Point_Generator()
+            for (int i = 0; i < list_of_sportsmens.Length; i++)
             {
-                int[] points = new int[4]; double total_points = 0;
-                Random point = new Random();
-                for (int i = 0; i < 4; i++)
-                {
-                    points[i] = point.Next(0, 42);
-                }
-
-                //поиск максимума
-                int max = int.MinValue; int imax = 0;
-                for (int i = 0; i < 4; i++)
-                {
-                    if (points[i] > max)
-                    {
-                        max = points[i]; imax = i;
-                    }
-                }
-
-                //поиск минимума 
-                int min = int.MaxValue; int imin = 0;
-                for (int i = 0; i < 4; i++)
-                {
-                    if (points[i] < min)
-                    {
-                        min = points[i]; imin = i;
-                    }
-                }
-
-                int sum = 0;
-                for (int i = 0; i < 4; i++)
-                {
-                    if (i != imax && i != imin) sum += points[i];
-                }
-
-                //финальные очки
-                total_points = sum * K();
-
-                return total_points;
+                Console.WriteLine($"имя: {list_of_sportsmens[i].Surname} очки: {list_of_sportsmens[i].Points}");
             }
-            static double K()
+            Sort(list_of_sportsmens);
+            Console.WriteLine("список спортсменов по местам");
+            for (int i = 0; i < list_of_sportsmens.Length; i++)
             {
-                double k = 2.5;
-                double[] k_list = new double[6];
-                for (int i = 0; i < 6; i++)
-                {
-                    k_list[i] = k;
-                    k += 0.2;
-                }
-                Random random = new Random();
-                int K = random.Next(0, 5);
-                k = k_list[K];
-                return k;
+                Console.WriteLine($"имя: {list_of_sportsmens[i].Surname} очки: {list_of_sportsmens[i].Points}");
             }
-            static Sportsmen[] Sort(Sportsmen[] sportsmens)
-            {
-                Sportsmen man;
-                for (int i = 0; i < sportsmens.Length; i++)
-                {
-                    for (int j = i + 1; j < sportsmens.Length; j++)
-                    {
-                        if (sportsmens[j].Points > sportsmens[i].Points)
-                        {
-                            man = sportsmens[i];
-                            sportsmens[i] = sportsmens[j];
-                            sportsmens[j] = man;
-                        }
-                    }
 
-                }
-                return sportsmens;
-            }
         }
+        static double Point_Generator()
+        {
+            int[] points = new int[4]; double total_points = 0;
+            Random point = new Random();
+            for (int i = 0; i < 4; i++)
+            {
+                points[i] = point.Next(0, 42);
+            }
 
+            //поиск максимума
+            int max = int.MinValue; int imax = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                if (points[i] > max)
+                {
+                    max = points[i]; imax = i;
+                }
+            }
+
+            //поиск минимума 
+            int min = int.MaxValue; int imin = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                if (points[i] < min)
+                {
+                    min = points[i]; imin = i;
+                }
+            }
+
+            int sum = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                if (i != imax && i != imin) sum += points[i];
+            }
+
+            //финальные очки
+            total_points = sum * K();
+
+            return total_points;
+        }
+        static double K()
+        {
+            double k = 2.5;
+            double[] k_list = new double[6];
+            for (int i = 0; i < 6; i++)
+            {
+                k_list[i] = k;
+                k += 0.2;
+            }
+            Random random = new Random();
+            int K = random.Next(0, 5);
+            k = k_list[K];
+            return k;
+        }
+        static Sportsmen[] Sort(Sportsmen[] sportsmens)
+        {
+            Sportsmen man;
+            for (int i = 0; i < sportsmens.Length; i++)
+            {
+                for (int j = i + 1; j < sportsmens.Length; j++)
+                {
+                    if (sportsmens[j].Points > sportsmens[i].Points)
+                    {
+                        man = sportsmens[i];
+                        sportsmens[i] = sportsmens[j];
+                        sportsmens[j] = man;
+                    }
+                }
+
+            }
+            return sportsmens;
+        }
     }
 }
