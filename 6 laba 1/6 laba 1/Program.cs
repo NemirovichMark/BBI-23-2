37d368601d7960839ec021c899b2efc15ea05917
EkaterinaPushkareva04 <159522082+EkaterinaPushkareva04@users.noreply.github.com>
2024-03-01 11:16:25 +0300

    Add files via upload

---
+++ 6 laba 1/6 laba 1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+class Competitor
+{
+    private string LastName;
+    private string Society;
+    private int Jump1;
+    private int Jump2;
+    private int TotalJump;
+
+    public Competitor(string lastName, string society, int jump1, int jump2)
+    {
+        LastName = lastName;
+        Society = society;
+        Jump1 = jump1;
+        Jump2 = jump2;
+        TotalJump = jump1 + jump2;
+    }
+
+    private int GetTotalJump()
+    {
+        return TotalJump;
+    }
+
+    public static void SortCompetitorsByTotalJump(ref Competitor[] competitors)
+    {
+        for (int i = 0; i < competitors.Length - 1; i++)
+        {
+            for (int j = i + 1; j < competitors.Length; j++)
+            {
+                if (competitors[i].GetTotalJump() < competitors[j].GetTotalJump())
+                {
+                    Competitor temp = competitors[i];
+                    competitors[i] = competitors[j];
+                    competitors[j] = temp;
+                }
+            }
+        }
+    }
+
+    public void DisplayCompetitor()
+    {
+        Console.WriteLine($"{LastName}\t{Society}\t{Jump1}\t{Jump2}\t{TotalJump}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Competitor[] competitors = new Competitor[5]
+        {
+            new Competitor("Ivanov", "A", 5, 4),
+            new Competitor("Petrov", "B", 4, 3),
+            new Competitor("Cidorov", "C", 2, 3),
+            new Competitor("Dorokov", "D", 3, 3),
+            new Competitor("Epihov", "E", 1, 2),
+        };
+
+        Competitor.SortCompetitorsByTotalJump(ref competitors);
+
+        Console.WriteLine("Фамилия\tОбщество\tПопытка 1\tПопытка 2\tСумма баллов");
+        foreach (var competitor in competitors)
+        {
+            competitor.DisplayCompetitor();
+        }
+    }
+}
---
+++ 6 laba 3/6 laba 3/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+struct Team
+{
+    private string name;
+    private int _GoalsScored;
+    private int _GoalsConceded;
+    private int _Points;
+
+    public string Name => name;
+    public int GoalsScored => _GoalsScored;
+    public int GoalsConceded => _GoalsConceded;
+    public int Points => _Points;
+
+    public Team(string name, int GoalsScored, int GoalsConceded)
+    {
+        this.name = name;
+        this._GoalsScored = GoalsScored;
+        this._GoalsConceded = GoalsConceded;
+        _Points = 0;
+    }
+
+    public void PlayMatch(int result)
+    {
+        if (result == 1)
+        {
+            _Points += 3;
+        }
+        else if (result == 0)
+        {
+            _Points += 0;
+        }
+        else
+        {
+            _Points += 1;
+        }
+    }
+
+    public string PrintTeamInfo()
+    {
+        return $"{Name}: {Points} очков";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Team[] teams = new Team[6];
+        teams[0] = new Team("Винлайн", 1, 1);
+        teams[1] = new Team("ВанВин", 3, 1);
+        teams[2] = new Team("Лидер", 5, 2);
+        teams[3] = new Team("Чемпион", 4, 3);
+        teams[4] = new Team("Звезда", 3, 3);
+        teams[5] = new Team("Герой", 1, 3);
+
+        for (int i = 0; i < teams.Length; i++)
+        {
+            for (int j = i + 1; j < teams.Length; j++)
+            {
+                int scoreTeam1 = teams[i].GoalsScored - teams[j].GoalsConceded;
+                int scoreTeam2 = teams[j].GoalsScored - teams[i].GoalsConceded;
+
+                if (scoreTeam1 > scoreTeam2)
+                {
+                    teams[i].PlayMatch(3);
+                    teams[j].PlayMatch(0);
+                }
+                else if (scoreTeam1 < scoreTeam2)
+                {
+                    teams[i].PlayMatch(0);
+                    teams[j].PlayMatch(3);
+                }
+                else
+                {
+                    teams[i].PlayMatch(1);
+                    teams[j].PlayMatch(1);
+                }
+            }
+        }
+
+
+        Array.Sort(teams, (x, y) =>
+        {
+            if (x.Points != y.Points)
+            {
+                return y.Points.CompareTo(x.Points);
+            }
+            else
+            {
+                return (y.GoalsScored - y.GoalsConceded).CompareTo(x.GoalsScored - x.GoalsConceded);
+            }
+        });
+
+        Console.WriteLine("Таблица результатов:");
+        for (int i = 0; i < teams.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}. {teams[i].PrintTeamInfo()}");
+        }
+    }
+}
