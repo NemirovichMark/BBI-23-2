acd2f9c4a548a0da6f0622afc2f362b610ecc54a
ArmenArakelyan <161484880+ArmenArakelyan@users.noreply.github.com>
2024-03-22 12:30:48 +0300

    Add files via upload

---
+++ кр1.аракелян армен. 1 номер .cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KR
+{
+    struct Book
+    {
+        private static int ISBN;
+        private int _isbn;
+        private string _name;
+        private string _author;
+        private int _year;
+        public Book(string name, string author, int year)
+        {
+            _isbn = ISBN;
+            ISBN++;
+            _name = name;
+            _author = author;
+            _year = year;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"{_isbn}. '{_name}' - {_author}, {_year}г.");
+        }
+        public static string OneAuthor(Book[] arr, string a)
+        {
+            string res = "";
+            foreach (Book b in arr)
+            {
+                if (b._author == a) { res += $"{b._name}; "; }
+            }
+            if (res == "")
+            {
+                res = "Книг данного автора не найдено";
+            }
+            return res;
+        }
+        public static string OneYear(Book[] arr, int a)
+        {
+            string res = "";
+            foreach (Book b in arr)
+            {
+                if (b._year > (a - 1) * 100 && b._year <= a * 100) { res += $"{b._name}; "; }
+            }
+            if (res == "")
+            {
+                res = "Книг данного века не найдено";
+            }
+            return res;
+        }
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Book[] books = new Book[10];
+            books[0] = new Book("Анна Каренина", "Толстой", 1873);
+            books[1] = new Book("Гордость и предубеждение", "Остен", 1797);
+            books[2] = new Book("Евгений Онегин", "Пушкин", 1831);
+            books[3] = new Book("Мартин Иден", "Лондон", 1909);
+            books[4] = new Book("Братья Карамазовы", "Достоевский", 1880);
+            books[5] = new Book("Собор Парижской Богоматери", "Гюго", 1831);
+            books[6] = new Book("Малое собрание сочинений", "Лермонтов", 1835);
+            books[7] = new Book("Ромео и Джульетта", "Шекспир", 1595);
+            books[8] = new Book("Мастер и Маргарита", "Булгаков", 1928);
+            books[9] = new Book("Финансист", "Драйзер", 1912);
+            for (int i = 0; i < books.Length; i++)
+            {
+                books[i].Print();
+            }
+            Console.WriteLine("Введите фамилию автора:");
+            string aut = Console.ReadLine();
+            Console.WriteLine(Book.OneAuthor(books, aut));
+            Console.WriteLine("Введите век арабскими цифрами:");
+            int ye = int.Parse(Console.ReadLine());
+            Console.WriteLine(Book.OneYear(books, ye));
+        }
+    }
+}
---
+++ кр1.аракелян армен. 2 номер.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+abstract class Book : IComparable<Book>
+{
+    public string Title { get; }
+    public long ISBN { get; }
+    public string Author { get; }
+    public int Year { get; }
+    public decimal Price { get; }
+
+    public Book(string title, long isbn, string author, int year, decimal price)
+    {
+        Title = title;
+        ISBN = isbn;
+        Author = author;
+        Year = year;
+        Price = price;
+    }
+
+    public abstract decimal CalculatePrice();
+
+    public int CompareTo(Book other)
+    {
+        return other.Price.CompareTo(Price);
+    }
+
+    public virtual void DisplayInfo()
+    {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"ISBN: {ISBN}");
+        Console.WriteLine($"Author: {Author}");
+        Console.WriteLine($"Year: {Year}");
+        Console.WriteLine($"Price: {Price:C}");
+    }
+}
+
+class PaperBook : Book
+{
+    public bool HardCover { get; }
+
+    public PaperBook(string title, long isbn, string author, int year, decimal price, bool hardCover)
+        : base(title, isbn, author, year, price)
+    {
+        HardCover = hardCover;
+    }
+
+    public override decimal CalculatePrice()
+    {
+        return HardCover ? Price * 1.2m : Price;
+    }
+
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine($"Hard Cover: {HardCover}");
+    }
+}
+
+class ElectronicBook : Book
+{
+    public string Format { get; }
+
+    public ElectronicBook(string title, long isbn, string author, int year, decimal price, string format)
+        : base(title, isbn, author, year, price)
+    {
+        Format = format;
+    }
+
+    public override decimal CalculatePrice()
+    {
+        return Price;
+    }
+
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine($"Format: {Format}");
+    }
+}
+
+class AudioBook : Book
+{
+    public bool StudioRecorded { get; }
+
+    public AudioBook(string title, long isbn, string author, int year, decimal price, bool studioRecorded)
+        : base(title, isbn, author, year, price)
+    {
+        StudioRecorded = studioRecorded;
+    }
+
+    public override decimal CalculatePrice()
+    {
+        return StudioRecorded ? Price * 1.5m : Price;
+    }
+
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine($"Studio Recorded: {StudioRecorded}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Book[] books = new Book[]
+        {
+            new PaperBook("Анна Каренина", 9780743273565, "Толстой", 1873, 20.50m, true),
+            new ElectronicBook("Гордость и предубеждение", 9780061120084, "Остен", 1797, 15.99m, "pdf"),
+            new AudioBook("Евгений Онегин", 9780451524935, "Пушкин", 1831, 25.00m, true),
+            new PaperBook("Мартин Иден", 9780141439518, "Лондон", 1909, 18.75m, false),
+            new ElectronicBook("Братья Карамазовы", 9780590353427, "Достоевский", 1880, 12.99m, "epub"),
+            new AudioBook("Собор Парижской Богоматери", 9780316769488, "Гюго", 1831, 30.00m, false),
+            new PaperBook("Малое собрание сочинений", 9780618640157, "Лермонтов", 1835, 40.25m, true),
+            new ElectronicBook("Ромео и Джульетта", 9780547928227, "Шекспир", 1595, 9.99m, "fb2"),
+            new AudioBook("Мастер и Маргарита", 9780143058142, "Булгаков", 1928, 35.50m, true),
+            new PaperBook("Финансист", 9780486278070, "Драйзер", 1912, 22.99m, false)
+        };
+
+        var paperBooks = books.OfType<PaperBook>().OrderByDescending(b => b.Price).ToList();
+        var electronicBooks = books.OfType<ElectronicBook>().OrderByDescending(b => b.Price).ToList();
+        var audioBooks = books.OfType<AudioBook>().OrderByDescending(b => b.Price).ToList();
+
+        Console.WriteLine("Paper Books:");
+        foreach (var book in paperBooks)
+        {
+            book.DisplayInfo();
+            Console.WriteLine();
+        }
+        Console.WriteLine("Electronic Books:");
+        foreach (var book in electronicBooks)
+        {
+            book.DisplayInfo();
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Audio Books:");
+        foreach (var book in audioBooks)
+        {
+            book.DisplayInfo();
+            Console.WriteLine();
+        }
+
+        var allBooks = books.OrderByDescending(b => b.Price).ToList();
+
+        Console.WriteLine("All Books:");
+        foreach (var book in allBooks)
+        {
+            book.DisplayInfo();
+            Console.WriteLine();
+        }
+    }
+}
