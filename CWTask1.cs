8db0f8ede97ceb534cbaeb129edf2d29e0c6804b
VoronovaAleksandra <159521678+VoronovaAleksandra@users.noreply.github.com>
2024-03-22 12:17:30 +0300

    3 Вариант

---
+++ CWTask1.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+
+struct Disiple
+{
+    public string Name;
+    public int Age;
+    public int[] Grades;
+    public double AverageGrade;
+    public bool StraightAStudent;
+    private double CalculateAverageGrade()
+    {
+        int summ = 0;
+        foreach (int grades in Grades)
+        {
+            summ += grades;
+        }
+        return (double)summ / Grades.Length;
+    }
+
+    public Disiple(string name, int age, int[] grades, double averagegrade)
+    {
+        Name = name;
+        Age = age;
+        Grades = grades;
+        AverageGrade = averagegrade;
+        StraightAStudent = true;
+        
+    }
+    public void DisplayInfo()
+    {
+        bool status;
+        if(AverageGrade>4.5)
+        {
+            status = StraightAStudent;
+        }
+        else
+        {
+            status = false;
+        }
+      if(status==true)
+        {
+            Console.WriteLine($"{Name} {Age} {AverageGrade} {status}");
+        }
+       else
+        {
+            Console.WriteLine($"{Name} {Age} {AverageGrade} ");
+        }
+}
+class Program
+{
+    static void Main()
+    {
+        double[] grades1 = new double[5] { 2, 4, 5, 5, 3 };
+        double[] grades2 = new double[5] { 3, 3, 5, 4, 3 };
+        double[] grades3 = new double[5] { 5, 5, 4, 5, 3 };
+        double[] grade4 = new double[5]  {4, 3, 2, 5, 5 };
+            
+    }
+
+    }
